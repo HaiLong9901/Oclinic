4eebc5d967d08905437055e2b5e6bfef3cf88668
unknown <dohailong9901@gmail.com>
2021-12-27 00:45:54 +0700

    dropdown

--- src/app/controllers/doctorController.js
+++ src/app/controllers/doctorController.js
@@ -9,11 +9,30 @@ class DoctorController{
             for(let value of doctors){
                 display.push(value.dataValues);
             }
-            console.log('----------------');
-            console.log({display});
-            console.log('----------------');
+            // console.log('----------------');
+            // console.log({display});
+            // console.log('----------------');
             // res.json({display});
             res.render('doctor', {display});
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    surgery = async (req, res) => {
+        try{
+            let doctors = await doctor.doctor.findAll({
+                where:{
+                    id_dep:'DEP1'
+                }
+            });
+            let display = [];
+            for(let value of doctors){
+                display.push(value.dataValues);
+            };
+            console.log({display});
+            res.render('surgery', {display});
+            // res.json(display);
         }catch(error){
             console.log(error);
         }
--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const doctorController = require('../app/controllers/doctorController');
 
+router.get('/surgery', doctorController.surgery);
 router.use('/', doctorController.index);
 
 module.exports = router;
